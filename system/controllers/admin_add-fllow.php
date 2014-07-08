5de9e4a2a84b72c2aa338c01dcd1291ba09f7ecc
Masoud A <amini141@gmail.com>
2014-07-08 12:59:42 +0430

    Update admin_add-fllow.php

--- system/controllers/admin_add-fllow.php
+++ system/controllers/admin_add-fllow.php
@@ -6,7 +6,7 @@
 	if( !$this->user->is_logged ) {
 		$this->redirect('signin');
 	}
-	$db2->query('SELECT 1 FROM users WHERE id="'.$this->user->id.'" AND is_network_admin="1" AND is_admin_boss="1" LIMIT 1');
+	$db2->query('SELECT 1 FROM users WHERE id="'.$this->user->id.'" AND is_network_admin="1"  LIMIT 1');
 	if( 0 == $db2->num_rows() ) {
 		$this->redirect('dashboard');
 	}
