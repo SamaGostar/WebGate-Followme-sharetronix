4bdb424b656f05080a9c880281f86a8190d977c3
Masoud A <amini141@gmail.com>
2014-07-13 16:57:19 +0430

    Update payfllow.php

--- themes/default/html/payfllow.php
+++ themes/default/html/payfllow.php
@@ -1,7 +1,7 @@
 <?php
-	
-	$this->load_template('header.php');
-	
+
+$this->load_template('header.php');
+
 ?>
 
 <? if($D->error) { ?>
@@ -10,13 +10,13 @@
 
 
 
-<? if($D->sabad && $n=  $D->baste){ ?>
+<? if($D->sabad && $n= $D->baste){ ?>
 
-<?= msgbox ('خرید بسته '.$n['pri'].' ماهه با قیمت '.$n['amount'].' تومان', nl2br('<b>
+<?= msgbox ('خرید بسته '.$n['pri'].' ماهه با قیمت '.$n['amount'].' تومان', ('<b><br>
 در اینجا شما با خرید بسته مذکور ، اطلاعیه خود جهت جذب افزاد به پروفایل و دنبال کردن را فعال میکنید.
-
-آواتار شما و دکمه دنبال کردن شما در پروفایل با عنوان کاربران اماده دنبال شدن اضافه می شود.
-</b>
+<br>
+آواتار شما و دکمه دنبال کردن شما در پروفایل با عنوان کاربران اماده دنبال شدن اضافه می شود.<br>
+</b><br><br>
 <center>
 <form action="'.$C->SITE_URL.'payfllow/tab:submit" method="POST" >
 <input type="hidden" value="'.$n['pri'].'" name="pri_2"/>
@@ -24,7 +24,7 @@
 <input type="submit" value="پرداخت نهایی" name="pardakht"/>
 </form>
 </center>
-',false))?>
+'),false)?>
 
 
 
@@ -34,7 +34,7 @@
 
 
 <?php
-	
-	$this->load_template('footer.php');
-	
+
+$this->load_template('footer.php');
+
 ?>
